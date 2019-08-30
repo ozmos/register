2d60988ad454cf9c5af1aa1705544f1c091fd98e
ozmos <osamumorozumi@gmail.com>
2019-08-30 19:48:41 +0800

    added error/success messages on change event for check box

--- js/errorMessages.js
+++ js/errorMessages.js
@@ -3,6 +3,7 @@
 
 // This function adds the error message.
 // It takes two arguments: the form element ID and the message.
+
 function addErrorMessage(id, msg) {
    	'use strict';
     
@@ -21,12 +22,16 @@ function addErrorMessage(id, msg) {
         // Create the span:
         span = document.createElement('span');
         span.id = newId;
+        //adding error class to span
+       
 		span.classList.add('error');
         span.appendChild(document.createTextNode(msg));
         
         // Add the span to the parent:
         elem.parentNode.appendChild(span);
-        elem.previousSibling.classList.add('error');
+        // adding error class to label
+       
+        elem.previousElementSibling.classList.add('error');
 
     } // End of main IF-ELSE.
 
@@ -42,11 +47,63 @@ function removeErrorMessage(id) {
 	if (span) {
     
 	    // Remove the class from the label:
-	    span.previousSibling.previousSibling.classList.remove('error');
-    
+	    span.previousElementSibling.previousElementSibling.classList.remove('error');
+        
 	    // Remove the span:
 	    span.parentNode.removeChild(span);
-
+        
 	} // End of IF.
     
 } // End of removeErrorMessage() function.
+
+function addSuccessMessage(id) { 
+    'use strict';
+
+    // Get the form element reference:
+    var elem = document.getElementById(id);
+    
+    // Define the new span's ID value:
+    var newId = id + 'Success';
+    
+    // Check for the existence of the span:
+    var span = document.getElementById(newId);
+    if (span) {
+        span.firstChild.value = '\u2714'; // Update
+    } else { // Create new.
+    
+        // Create the span:
+        span = document.createElement('span');
+        span.id = newId;
+        // add success class
+		span.classList.add('success');
+        span.appendChild(document.createTextNode('\u2714'));
+        
+        // Add the span to the parent:
+        elem.parentNode.appendChild(span);
+        // add success class
+       
+        elem.previousElementSibling.classList.add('success');
+
+    } // End of main IF-ELSE.
+   
+} //end of addSuccessMessage Function
+
+function removeSuccessMessage (id) {
+   
+    'use strict';
+
+    // Get a reference to the span:
+    var span = document.getElementById(id + 'Success');
+    if (span) {
+    
+        // Remove the class from the label:
+        span.previousElementSibling.previousElementSibling.classList.remove('success');
+        
+        // Remove the span:
+        span.parentNode.removeChild(span);
+     
+    } // End of IF.
+    
+
+
+} //end of removeSuccessMessage function
--- js/register.js
+++ js/register.js
@@ -23,32 +23,39 @@ function validateForm(e) {
 
 	// Flag variable:
 	var error = false;
-	function validateTextField (field, regex, message) {
-		'use strict';
 
+	// re-useable validation function
+	function validateTextField (field, regex) {
+		'use strict';
+		// trim leading/trailing white space before passing testing with regex
 		if (regex.test(field.value.trim())) {
 			removeErrorMessage(field.name);
+			addSuccessMessage(field.name);
 		} else {
-			addErrorMessage(field.name, message);
+			removeSuccessMessage(field.name);
+			addErrorMessage(field.name, `Please enter your ${field.dataset.message}`);
 			error = true;
 		} 
+		
 	}
 	
 
 	// Validate the first name:
-	validateTextField(firstName, /^[a-z ,.'\-]{2,30}$/i, 'Please enter letters, commas, spaces and hyphens only.' );
+	validateTextField(firstName, /^[a-z ,.'\-]{2,30}$/i);
 	// Validate the last name:
-	validateTextField(lastName, /^[a-z ,.'\-]{2,30}$/i, 'Please enter letters, commas, spaces and hyphens only.');
+	validateTextField(lastName, /^[a-z ,.'\-]{2,30}$/i);
 	// Validate the email address:
-	validateTextField(email, /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$/, 'Please enter a valid email address.');
+	validateTextField(email, /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$/);
 	// Validate the phone number:
-	validateTextField(phone, /\d{3}[ \-]?\d{3}[ \-]?\d{4}/, 'Please enter your phone number in the format XXX XXX XXX, XXX-XXX-XXXX or XXXXXXXXXX.');
+	validateTextField(phone, /\d{3}[ \-]?\d{3}[ \-]?\d{4}/);
 	// Validate City:
-	validateTextField(city, /^[a-z ,.'\-]{2,40}$/i, 'Please enter letters, commas, spaces, hyphens  and full-stops only.');
+	validateTextField(city, /^[a-z ,.'\-]{2,40}$/i);
 	// Validate the state:
 	if (state.selectedIndex != 0) {
 		removeErrorMessage('state');
+		addSuccessMessage('state');
 	} else {
+		removeSuccessMessage('state');
 		addErrorMessage('state', 'Please select your state.');
 		error = true;
 	}
@@ -89,8 +96,9 @@ function validateForm(e) {
 	// validates depending if state has been selected
 	if (state.selectedIndex != 0) {
 		removeErrorMessage('zip');
-		validateTextField(zip, postCodeRegex(state.value), 'Please enter your post code starting with the correct digit and with exactly 4 digits.');
+		validateTextField(zip, postCodeRegex(state.value));
 	} else {
+		removeSuccessMessage('zip');
 		addErrorMessage('zip', 'Please select your state before you enter your post code');
 		error = true;
 	}
@@ -111,22 +119,51 @@ function validateForm(e) {
     
 } // End of validateForm() function.
 
+// Function to get correct field name for error message 
+function fieldName (id) {
+	//check if id is camel cased
+	var regex = /[A-Z]/;
+	var splitPoint = /([A-Z])/g;
+	if (regex.test(id)) {
+		//split at uppercase see https://stackoverflow.com/questions/18379254/regex-to-split-camel-case
+		return id.replace(splitPoint, ' $1').toLowerCase();
+	} return id;
+}
+// Function to add or remove error messages when terms checkbox changes
+function toggleErrors() {
+	var inputs = Array.from(document.getElementsByClassName('field'));
+	// toggle error class on any empty fields
+	//check for any empty fields
+	var emptyFields = inputs.filter(field => field.value === '');
+	if (emptyFields.length > 0) error = true;
+	// toggle error class on empty fields
+	
+	emptyFields.forEach(field => {
+		!field.classList.contains('error') && field.classList.add('error');
+		addErrorMessage(field.id, 'Please enter your ' + fieldName(field.name)); //TODO: use previousElementChild.innerHtml to get name value
+	});
+}
 // Function called when the terms checkbox changes.
 // Function enables and disables the submit button.
 function toggleSubmit() {
 	'use strict';
-    
+ 
 	// Get a reference to the submit button:
 	var submit = U.$('submit');
+	var checked = U.$('terms').checked;
 	
 	// Toggle its disabled property:
-	if (U.$('terms').checked) {
+	if (checked) {
 		submit.disabled = false;
+		validateForm();
 	} else {
 		submit.disabled = true;
+	
 	}
 	
 } // End of toggleSubmit() function.
+
+
 
 // Establish functionality on window load:
 window.onload = function() {
@@ -143,11 +180,10 @@ window.onload = function() {
 
 	// Enbable tooltips on all input elements:
 	var inputs = ['firstName', 'lastName', 'phone', 'email', 'city', 'state', 'zip'];
-	/* inputs.forEach(id => U.enableTooltips(id)); */
-	es5: 
+	
 	inputs.forEach(function(id) {
 		U.enableTooltips(id);
 	});
 	
     
-};
+}
